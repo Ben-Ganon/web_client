828d62a21d3985e0b0bbdef941e7b2b728f65e43
BigG97 <92310078+BigG97@users.noreply.github.com>
2022-03-29 16:14:20 +0300

    next

--- src/login.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import ReactDOM, { unmountComponentAtNode } from 'react-dom';
-// import './index.css';
-// import App from './App';
-
-validUsers = new Map()
-validUsers.set("ben", "1234")
-console.log(validUsers.get("ben"))

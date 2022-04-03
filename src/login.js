5a090595628056073edb1175ee6110d53909a820
BigG97 <92310078+BigG97@users.noreply.github.com>
2022-04-03 15:16:25 +0300

    login - ben

--- src/login.js
+++ src/login.js
@@ -3,17 +3,19 @@
 function Login() {
     return (
         <body>
-                <h1>
-                    Welcome to FreakNet!
-                </h1> 
-                <form>
-                    <label for="uname"> Username: </label>
-                    <input type="text" placeholder="your username here" id="username"></input>  <br/>
-                    <label for="psswd">Password: </label>
-                    <input type="text" placeholder="your password here" id="password" /> <br></br> <br></br>
-                    <input type="button" value="     go     " onclick="window.location.href= 'https://www.google.com/';" />
-
-                </form>
+            <h1>
+                Welcome to FreakNet!
+            </h1>
+            <form>
+                <label for="uname"> Username: </label>
+                <input type="text" placeholder="your username here" id="username"></input>  <br />
+                <label for="psswd">Password: </label>
+                <input type="text" placeholder="your password here" id="password" /> <br></br> <br></br>
+                <input type="button" value="     go     " onclick="window.location.href= 'https://www.google.com/';" /><br></br>
+            </form>
+            <div>
+                <link href="https://www.google.com/" value="  not registered?  " />
+            </div>
         </body>
     );
 }

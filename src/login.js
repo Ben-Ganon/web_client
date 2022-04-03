4cee8db2ff8cd05169392f8d1d01f81ca74e5620
BigG97 <92310078+BigG97@users.noreply.github.com>
2022-04-03 14:55:20 +0300

    new commit

---
+++ src/login.js
@@ -0,0 +1,21 @@
+
+
+function Login() {
+    return (
+        <body>
+                <h1>
+                    Welcome to FreakNet!
+                </h1> 
+                <form>
+                    <label for="uname"> Username: </label>
+                    <input type="text" placeholder="your username here" id="username"></input>  <br/>
+                    <label for="psswd">Password: </label>
+                    <input type="text" placeholder="your password here" id="password" /> <br></br> <br></br>
+                    <input type="button" value="     go     " onclick="window.location.href= 'https://www.google.com/';" />
+
+                </form>
+        </body>
+    );
+}
+
+export default Login;

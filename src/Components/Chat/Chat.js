e2179c77ffb9d8eb5de297430fe8976a52ed1ad7
Your Name <you@example.com>
2022-04-12 14:07:25 +0300

    just small miss char

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -79,7 +79,7 @@ export default function Chat() {
                   </div>
                 </div>
               </div>
-            </div>2
+            </div>
           </div>
         </div>
       </div>

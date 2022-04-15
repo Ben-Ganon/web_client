1e1e7381098f056808e13b5c42a32f2101a7f094
BigG97 <92310078+BigG97@users.noreply.github.com>
2022-04-15 12:04:11 +0300

    Login as Function instead of class

--- src/Components/Registration+Login/Login.js
+++ src/Components/Registration+Login/Login.js
@@ -3,69 +3,134 @@ import App, { buttonLogin } from "../App";
 import { Link } from "react-router-dom";
 import { Form, Button, Container, Col, Row, Card, Alert } from "react-bootstrap";
 import users from "../Users";
-class Login extends React.Component {
-    constructor() {
-        super();
-        this.state = { accessSuccess: false, accessFail: false , suersList : users};
-        this.checkLogin = this.checkLogin.bind(this);
-    }
 
-    checkLogin() {
+
+
+
+export default function Chat() {
+
+
+    const [accessSuccess, setSuccess] = useState(false);
+    const [accessFail, setFail] = useState(false);
+    const [lUsers, setUsers] = useState(users);
+
+    const  checkLogin = ()=> {
         let password = (document.getElementById("formPassword")).value;
         let username = (document.getElementById("formUsername")).value;
-        console.log(this.state.suersList)
+        console.log(lUsers)
         console.log(users)
-        if (this.state.suersList.has(username) && this.state.suersList.get(username).at(0) == password) {
-            this.setState({ accessSuccess: true });
+        if (lUsers.has(username) && lUsers.get(username).at(0) == password) {
+            setSuccess(true);
             setTimeout(() => { window.location.replace('/Chat')}, 500);
         }
         else {
-            this.setState({ accessFail: true });
+            setFail(true);
         }
     }
-    render() {
 
-        return (
-            <div className="text-center" style={{ marginLeft: "40%", marginTop: "10%" }}>
+    
+    return(
+        <div className="text-center" style={{ marginLeft: "40%", marginTop: "10%" }}>
 
-                <Card style={{ width: '20rem' }}>
-                    <h1>
-                        Welcome to FreakNet!
-                    </h1>
+        <Card style={{ width: '20rem' }}>
+            <h1>
+                Welcome to FreakNet!
+            </h1>
 
-                    <Form.Group className="loginForm" controlId="formUsername">
-                    <br/>
-                        <Form.Control type="username" placeholder="Enter Username"></Form.Control>
-                    </Form.Group>
-
-
-                    <Form.Group className="loginForm" controlId="formPassword">
-                    <br/>
-                        <Form.Control type="password" placeholder="Enter Password"></Form.Control>
-                    </Form.Group>
+            <Form.Group className="loginForm" controlId="formUsername">
+            <br/>
+                <Form.Control type="username" placeholder="Enter Username"></Form.Control>
+            </Form.Group>
 
 
-                    <br/>
-                    <Button variant="primary" type="submit" onClick={this.checkLogin} class="buttonLogin">
-                        Submit
-                    </Button>
+            <Form.Group className="loginForm" controlId="formPassword">
+            <br/>
+                <Form.Control type="password" placeholder="Enter Password"></Form.Control>
+            </Form.Group>
 
-                    <Alert show={this.state.accessSuccess} onClose={() => { setTimeout(() => { this.setState({ accessSuccess: false }) }, 1) }} variant="primary" dismissible>
-                        <Alert.Heading>Access Granted!</Alert.Heading>
-                    </Alert>
-                    <Alert show={this.state.accessFail} onClose={() => this.setState({ accessFail: false })} variant="danger" dismissible>
-                        <Alert.Heading>Wrong Username or Password</Alert.Heading>
-                    </Alert>
 
-                </Card>
-                <br />
-                <Link to="/Register" style={{ marginLeft: "-60%" }}>Not Registered?</Link>
-            </div>
-        );
-    }
+            <br/>
+            <Button variant="primary" type="submit" onClick={()=> (checkLogin())} class="buttonLogin">
+                Submit
+            </Button>
+
+            <Alert show={accessSuccess} onClose={() => ( setSuccess(false))} variant="primary" dismissible>
+                <Alert.Heading>Access Granted!</Alert.Heading>
+            </Alert>
+            <Alert show={accessFail} onClose={() => setFail(false)} variant="danger" dismissible>
+                <Alert.Heading>Wrong Username or Password</Alert.Heading>
+            </Alert>
+
+        </Card>
+        <br />
+        <Link to="/Register" style={{ marginLeft: "-60%" }}>Not Registered?</Link>
+    </div>
+    );
 }
 
-export default Login;
+
+// class Login extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = { accessSuccess: false, accessFail: false , suersList : users};
+//         this.checkLogin = this.checkLogin.bind(this);
+//     }
+
+//     checkLogin() {
+//         let password = (document.getElementById("formPassword")).value;
+//         let username = (document.getElementById("formUsername")).value;
+//         console.log(this.state.suersList)
+//         console.log(users)
+//         if (this.state.suersList.has(username) && this.state.suersList.get(username).at(0) == password) {
+//             this.setState({ accessSuccess: true });
+//             setTimeout(() => { window.location.replace('/Chat')}, 500);
+//         }
+//         else {
+//             this.setState({ accessFail: true });
+//         }
+//     }
+//     render() {
+
+//         return (
+//             <div className="text-center" style={{ marginLeft: "40%", marginTop: "10%" }}>
+
+//                 <Card style={{ width: '20rem' }}>
+//                     <h1>
+//                         Welcome to FreakNet!
+//                     </h1>
+
+//                     <Form.Group className="loginForm" controlId="formUsername">
+//                     <br/>
+//                         <Form.Control type="username" placeholder="Enter Username"></Form.Control>
+//                     </Form.Group>
+
+
+//                     <Form.Group className="loginForm" controlId="formPassword">
+//                     <br/>
+//                         <Form.Control type="password" placeholder="Enter Password"></Form.Control>
+//                     </Form.Group>
+
+
+//                     <br/>
+//                     <Button variant="primary" type="submit" onClick={this.checkLogin} class="buttonLogin">
+//                         Submit
+//                     </Button>
+
+//                     <Alert show={this.state.accessSuccess} onClose={() => { setTimeout(() => { this.setState({ accessSuccess: false }) }, 1) }} variant="primary" dismissible>
+//                         <Alert.Heading>Access Granted!</Alert.Heading>
+//                     </Alert>
+//                     <Alert show={this.state.accessFail} onClose={() => this.setState({ accessFail: false })} variant="danger" dismissible>
+//                         <Alert.Heading>Wrong Username or Password</Alert.Heading>
+//                     </Alert>
+
+//                 </Card>
+//                 <br />
+//                 <Link to="/Register" style={{ marginLeft: "-60%" }}>Not Registered?</Link>
+//             </div>
+//         );
+//     }
+// }
+
 
 
 
